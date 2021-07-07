9d4ef6ab80552b04804cd41985651ea1949f615c
Vinicius Pereira <vguard.2003@gmail.com>
2021-07-07 18:54:19 -0300

    ...

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,29 @@
 import Modal from './modal.js'
 
 const modal = Modal()
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('modal button')
 
 //Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
+
 checkButtons.forEach (button => {
     button.addEventListener("click", event => {
         // Abrir modal
+        modalTitle.innerHTML = "Marcar como lido"
+        modalDescription.innerHTML = "Têm certeza que deseja Marcar como lido esta pergunta?"
         modal.open()
     })
 })
 
-//pegar quando o marcar como lido for clicado
+const deleteButtons = document.querySelectorAll(".actions a.delete")
 
+deleteButtons.forEach (button =>{
+    button.addEventListener("click", handleClick)
+})
 
-// Abrir modal
+function handleClick(){
+    modal.open()
+}
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,10 +1,17 @@
 export default function Modal(){
+
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.modal-wrapper .cancel')
+
+    cancelButton.addEventListener('click', close)
+
     function open(){
         //Funcionalidade de acrescentar active no código HTML
-        document.querySelector(".modal-wrapper").classList.add("active")
+        modalWrapper.classList.add("active")
     }
     function close(){
         //Funcionalidade de remover active no código HTML
+        modalWrapper.classList.remove("active")
     }
 
     return {
