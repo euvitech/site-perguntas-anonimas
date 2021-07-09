4cb99fd30ca4ac85ee0538732e41914d0fb6c190
Vinicius Pereira <vguard.2003@gmail.com>
2021-07-09 16:54:09 -0300

    ...

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -30,7 +30,7 @@ function handleClick(event, check = true){
     const questionId = event.target.dataset.id
 
     const form = document.querySelector(".modal form")
-    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML = `${text} Esta pergunta`
     modalDescription.innerHTML = check ? `Tem certeza que deseja ${text.toLowerCase()} está pergunta como lida?` : "Tem certeza que deseja excluir esta pergunta?"
