fccb6a24932e9787388457312da2e1c9ab571325
Vinicius Pereira <vguard.2003@gmail.com>
2021-07-09 16:54:09 -0300

    ...

---
+++ src/controllers/RoomController.js
@@ -0,0 +1,7 @@
+module.exports = {
+    create(req, res){
+        let = roomid = 1234567
+
+        res.redirect(`/rooms/${roomid}`)
+    }
+}
--- src/route.js
+++ src/route.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/rooms', (req, res) => res.render("rooms"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
-//route.get('/undership.com', (req, res) => res.render("index"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.post('/room/:room/:question/:action', QuestionController.index)
+route.get('/rooms/:room', (req, res) => res.render("rooms"))
+
+//Formato que o formulário dentro da modal têm que passar a informação
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 module.exports = route
