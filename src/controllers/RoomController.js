b213ceb344712874ff3797f6e8f8dcfee2233610
Vinicius Pereira <vguard.2003@gmail.com>
2021-07-10 21:33:08 -0300

    Creating Database and generating password auto

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,11 @@
 module.exports = {
     create(req, res){
-        let = roomid = 1234567
+        let roomid 
+
+        for(var i = 0; i < 6; i++) {
+            i == 0 ? roomid = Math.floor(Math.random() * 10).toString() :
+            roomid += Math.floor(Math.random() * 10).toString()
+        }
 
         res.redirect(`/rooms/${roomid}`)
     }
