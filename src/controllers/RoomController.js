d39c8fa43f36a99705355dc707364281fb7366c8
Vinicius Pereira <vguard.2003@gmail.com>
2021-07-12 14:21:09 -0300

    Insert a room in Database and ckeck Id

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -4,20 +4,35 @@ module.exports = {
     async create(req, res){
         const db = await Database()
         const pass = req.body.password
-        let roomid 
-
+        let roomId
+        let isRoom = true
+        while (isRoom) {
+        /* generates number of room*/
         for(var i = 0; i < 6; i++) {
             i == 0 ? roomid = Math.floor(Math.random() * 10).toString() :
             roomid += Math.floor(Math.random() * 10).toString()
         }
 
-         await db.run(`INSERT INTO rooms (
-            id,
-            pass
-         )VALUES(
-             ${parseInt(roomid)},
-             ${pass}
-         )`)
+        /* check password */
+        const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+        isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId )
+        
+        if(!isRoom){
+             /* insert a room in Database*/
+             await db.run(`INSERT INTO rooms (
+                id,
+                pass
+             )VALUES(
+                 ${parseInt(roomid)},
+                 ${pass}
+             )`)
+             }
+
+        }
+
+
+
+       
 
         await db.close()
 
